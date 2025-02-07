c7b74544697fe16b00c6bf491bae9877f67f505a
Lucas Matos <39964162+lucasrmatos@users.noreply.github.com>
2025-02-07 12:17:27 +0000

    2 - riri go

---
+++ blockchain/target/debug/deps/blockchain-3199477d46ebaf0b.d
@@ -0,0 +1,5 @@
+/workspaces/codespaces-blank/blockchain/target/debug/deps/libblockchain-3199477d46ebaf0b.rmeta: src/main.rs
+
+/workspaces/codespaces-blank/blockchain/target/debug/deps/blockchain-3199477d46ebaf0b.d: src/main.rs
+
+src/main.rs:
---
+++ blockchain/target/debug/deps/blockchain-a63c23558d9a5b21.d
@@ -0,0 +1,5 @@
+/workspaces/codespaces-blank/blockchain/target/debug/deps/libblockchain-a63c23558d9a5b21.rmeta: src/main.rs
+
+/workspaces/codespaces-blank/blockchain/target/debug/deps/blockchain-a63c23558d9a5b21.d: src/main.rs
+
+src/main.rs:
